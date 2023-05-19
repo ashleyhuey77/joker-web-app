d63b26cd01dde8ccbdc1815d3d776b81188e562d
Huey, Ashley <ashley.huey@warnermedia.com>
2023-05-19 10:02:10 -0400

    adding resize functionality to suites

--- client/src/components/views/suites/SuitesContent.js
+++ client/src/components/views/suites/SuitesContent.js
@@ -1,25 +1,56 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '../../../styles/Suites.css';
-import { Resizable } from 're-resizable';
+
+let isDragging = false;
+
 export function SuitesContent() {
+  const [cursor, setCursor] = useState("");
+  const [gridTemplateColumns, setGridTemplateColumns] = useState("");
+  function endDrag() {
+    isDragging = false;
+    setCursor("auto");
+  }
+
+  function startDrag() {
+    isDragging = true;
+    setCursor("ew-resize");
+  }
+
+  function drag(event) {
+    if(isDragging) {
+      let page = document.getElementById("page");
+      let leftcol = document.getElementById("leftcol");
+      let rightcol = document.getElementById("rightcol");
+
+      let leftColWidth = event.clientX;
+      let rightColWidth = page.clientWidth - event.clientX;
+
+      let dragbarWidth = 6;
+
+      let cols = [
+        leftColWidth,
+        dragbarWidth,
+        rightColWidth
+      ];
+
+      let newColDefn = cols.map(c => c.toString() + "px").join(" ");
+
+      setGridTemplateColumns(newColDefn);
+
+      event.preventDefault()
+    }
+  }
   return (
     <>
-      <Resizable
-        defaultSize={{
-          width: 320,
-          height: 200,
-        }}
-      >
-        Test Suites
-      </Resizable>
-      <Resizable
-        defaultSize={{
-          width: 320,
-          height: 200,
-        }}
-      >
-        Test Steps
-      </Resizable>
+      <div id="page" onMouseMove={drag} onMouseUp={endDrag} style={{cursor, gridTemplateColumns}}>
+        <div id="leftcol">
+          Test Suites
+        </div>
+        <div onMouseDown={startDrag} id="dragbar"></div>
+        <div id="rightcol">
+          Test Steps
+        </div>
+      </div>
     </>
   );
 }
