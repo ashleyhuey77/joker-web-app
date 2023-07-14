54a11dc90f60f9867bb6ca16e1f3a6c807673454
Ash Huey <127426909+ashleyhuey77@users.noreply.github.com>
2023-07-14 11:52:35 -0400

    Updates (#3)
    * add linting
    
    * add linting
    
    * adding server side code
    
    * fixing gitignore
    
    ---------
    
    Co-authored-by: Ash Huey <ashley.huey@meraki.net>

--- server/tests/test_steps/test_ts_model.py
+++ server/tests/test_steps/test_ts_model.py
@@ -1,26 +1,27 @@
 from unittest import TestCase
-from unittest.mock import patch
-
 from server.src.teststeps.test_steps_model import TestSteps
 
 
 class TestPerson(TestCase):
-    def testConstructPersonWithNoBirthDate(self):
-        with patch("flask_sqlalchemy._QueryProperty.__get__", autospec=True):
-            test_steps = TestSteps(
-                name="test_some_innocuous_thing", description="log in to the app"
-            )
+    def testConstructTestStepsWithNoOptionals(self):
+        test_steps = TestSteps(
+            name="test_some_innocuous_thing", description="log in to the app"
+        )
 
-            self.assertEqual("test_some_innocuous_thing", test_steps.name)
-            self.assertEqual("log in to the app", test_steps.description)
-            self.assertIsNone(test_steps.id)
+        self.assertEqual("test_some_innocuous_thing", test_steps.name)
+        self.assertEqual("log in to the app", test_steps.description)
+        self.assertIsNone(test_steps.id)
 
-    def testConstructPersonWithBirthDate(self):
-        with patch("flask_sqlalchemy._QueryProperty.__get__", autospec=True):
-            person = TestSteps(
-                name="test_some_innocuous_thing", description="log in to the app"
-            )
+    def testConstructTestStepsWithAllOptionals(self):
+        test_steps = TestSteps(
+            name="test_some_innocuous_thing",
+            description="log in to the app",
+            priority="LOTS",
+            state="DONE",
+        )
 
-            self.assertEqual("john", person.name)
-            self.assertEqual("01/01/1975", person.birth_date)
-            self.assertIsNone(person.id)
+        self.assertEqual("test_some_innocuous_thing", test_steps.name)
+        self.assertEqual("log in to the app", test_steps.description)
+        self.assertEqual("LOTS", test_steps.priority)
+        self.assertEqual("DONE", test_steps.state)
+        self.assertIsNone(test_steps.id)
