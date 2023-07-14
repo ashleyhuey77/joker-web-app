81f71741a132f18a6bcec9a297b3f5b2578adfee
Ash Huey <ashley.huey@meraki.net>
2023-07-14 15:42:04 -0400

    fixing test issues

--- client/jest.config.js
+++ client/jest.config.js
@@ -3,15 +3,12 @@ const jestConfig = {
   roots: ["<rootDir>"],
   verbose: true,
   testEnvironment: 'jest-environment-jsdom',
-  projects: ['<rootDir>'],
   testEnvironmentOptions: {
     url: 'http://localhost'
   },
-  transformIgnorePatterns: ['node_modules/(?!(@mui)/)'],
   snapshotSerializers: [
     "enzyme-to-json/serializer"
   ],
-  moduleDirectories: ["node_modules", 'src'],
   testMatch: ['*.test.js'],
   transform: {
     '^.+\\.(js|jsx|ts|tsx|mjs)$': 'babel-jest',
